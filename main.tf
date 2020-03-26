02f0113d05fa6d579b92661c6ee526dd7652def2
Vinoth Kumar K <vinothkaruppusamy@gmail.com>
2020-03-26 20:16:41 +0800

    Removed key pair and security group for demo.

--- main.tf
+++ main.tf
@@ -6,8 +6,6 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami             = "ami-2757f631"
   instance_type   = "t2.micro"
-  security_groups = ["default"]
-  key_name        = "default"
 
   tags {
     Name = "earth"
